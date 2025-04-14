c4fbc60a560db7ab98696fa19a55bfa0206b4ece
Devon Elmes <devonelmes@ufl.edu>
2025-04-14 12:04:45 -0700

    modified test_calc p2

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,21 @@
+# https://github.com/markszomstein/Lab10-MS-DE.git
+# Partner 1: Mark Szomstein
+# Partner 2: Devon Elmes
+
 import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(-5,10), 5, "Testing add with neg and pos int")
+        self.assertEqual(add(-2, -9), -11, "Testing add with two neg ints")
+        self.assertEqual(add(100,0.25), 100.25, "Testing add with pos int and float")
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
+    def test_subtract(self): # 3 assertions
+    	self.assertEqual(subtract(-5, 10), -15, "Testing subtract with neg and pos int")
+        self.assertEqual(subtract(-2, -9), 7, "Testing subtract with two neg ints")
+        self.assertEqual(subtract(100, 0.25), 99.75, "Testing subtract with pos int and float")
     # ##########################
 
     ######## Partner 1
@@ -19,18 +27,22 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     div(0, 5)
+        with self.assertRaises(ZeroDivisionError):
+            div(0, -4)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertAlmostEqual(logarithm(5,125), 3)
+        self.assertAlmostEqual(logarithm(3,81), 4)
+        self.assertAlmostEqual(logarithm(2,32), 5)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
+        with self.assertRaises(ValueError):
+            logarithm(0,125)
     # ##########################
     
     ######## Partner 1
