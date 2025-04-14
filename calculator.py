736b8b71b2cfb51c6511a4d5feb7083f77f4bc2a
Devon Elmes <devonelmes@ufl.edu>
2025-04-14 09:49:30 -0700

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -1,26 +1,31 @@
+# https://github.com/markszomstein/Lab10-MS-DE.git
+# Partner 1: Mark Szomstein
+# Partner 2: Devon Elmes
+
+
 import math
 
 
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def logarithm(a, b):
-    if a <= 0 or b <= 0:
-        raise ValueError("Logarithm base and argument must be positive")
+def log(a, b):
+    if a <= 0 or (b <= 0 and b != 1):
+        raise ValueError("Logarithm base and argument must be positive (and base not equal to 1)")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
 
 
