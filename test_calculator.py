dabc82bb6ef173b6c9c5e7afc333ddc107ce3f7f
Mark szomstein <markszomstein>
2025-04-14 14:27:59 -0400

    added unit tests

--- test_calculator.py
+++ test_calculator.py
@@ -10,14 +10,6 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
     #     # call division function inside, example:
@@ -32,24 +24,38 @@ class TestCalculator(unittest.TestCase):
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
-    
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+
+def test_multiply(self):
+    self.assertEqual(calculator.multiply(3, 4), 12)
+    self.assertEqual(calculator.multiply(-2, 5), -10)
+    self.assertEqual(calculator.multiply(0, 100), 0)
+
+def test_divide(self):
+    self.assertEqual(calculator.divide(2, 10), 5)
+    self.assertEqual(calculator.divide(4, 20), 5)
+    self.assertEqual(calculator.divide(1, 3), 3)
+
+def test_log_invalid_argument(self):
+    with self.assertRaises(ValueError):
+        calculator.logarithm(0, 5)
+
+def test_hypotenuse(self):
+    self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5.0)
+    self.assertAlmostEqual(calculator.hypotenuse(0, 5), 5.0)
+    self.assertAlmostEqual(calculator.hypotenuse(-3, -4), 5.0)
+
+def test_sqrt(self):
+    with self.assertRaises(ValueError):
+        calculator.square_root(-9)
+    self.assertAlmostEqual(calculator.square_root(16), 4.0)
+    self.assertAlmostEqual(calculator.square_root(0), 0.0)
+
+def test_divide_by_zero(self):
+    with self.assertRaises(ZeroDivisionError):
+        calculator.divide(0, 5)
+
 
 # Do not touch this
 if __name__ == "__main__":
